28a7e1eea85564024961a62f35f0744440904aa4
nitish bhardwaj <sis.nitish@gmail.com>
2023-05-27 09:42:28 +0530

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -14,7 +14,7 @@ export default function Footer() {
               href="https://github.com/geekynitish/"
               className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
             >
-              Patrika
+              GeekyNitish
             </a>
             <a
               href={`https://github.com/geekynitish/${EXAMPLE_PATH}`}
