d16b06018d3456f7630aebe6d69ede73d4cfea29
nitish bhardwaj <sis.nitish@gmail.com>
2023-05-27 09:36:15 +0530

    Nextjs-wordpress
    Nextjs-wordpress

---
+++ lib/constants.ts
@@ -0,0 +1,5 @@
+export const EXAMPLE_PATH = 'cms-wordpress'
+export const CMS_NAME = 'WordPress'
+export const CMS_URL = 'https://github.com/geekynitish'
+export const HOME_OG_IMAGE_URL =
+  'https://avatars.githubusercontent.com/u/3509486?v=4'
